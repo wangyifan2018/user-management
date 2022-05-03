3d9cf7f26516133522e1a519f0fa52eec6d45642
Yifan <372104922@qq.com>
2022-05-03 15:16:01 +0800

    chore: 重定位

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -11,12 +11,11 @@ export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-
       <Route path="user" element={<PostsList />} />
       <Route path="create" element={<AddPostForm />} />
-
       <Route path="posts/:postId" element={<SinglePostPage />} />
       <Route path="editPost/:postId" element={<EditPostForm />} />
+      <Route path="*" element={<HomePage />} />
     </Routes>
   );
 };
